1ea96c7d99cb0ad53d932c9d5f5e669ee3d088f0
Andrei Oprisan <andrei@Andreis-MacBook-Pro.local>
2013-12-23 12:56:15 -0500

    fix dependency issue

--- package.js
+++ package.js
@@ -3,11 +3,11 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.use(['bootstrap3', 'jquery'], 'client');
+    api.use(['jquery'], 'client');
 
 	api.add_files('lib/wysihtml5.js', 'client', {bare:true});  
-	api.add_files('lib/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5.css', 'client'); 
-	api.add_files('lib/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client', {bare:true}); //ok
-	api.add_files('lib/bootstrap-wysihtml5-0.0.2/bootstrap3-wysiwyg5-color.css', 'client'); //ok
-	api.add_files('lib/bootstrap-wysihtml5-0.0.2/bootstrap3-wysihtml5.js', 'client', {bare:true}); //ok
+	api.add_files('lib/bootstrap-wysihtml5.css', 'client'); 
+	api.add_files('lib/wysihtml5-0.3.0.js', 'client', {bare:true});
+	api.add_files('lib/bootstrap3-wysiwyg5-color.css', 'client');
+	api.add_files('lib/bootstrap3-wysihtml5.js', 'client', {bare:true});
 });

37d3311bb009e9720074f6c0ea3a1bc210cd31f4
Andrei Oprisan <andrei@oprisan.com>
2013-12-23 13:39:41 -0500

    fix wysiwyg js reference; release 0.3.2

--- package.js
+++ package.js
@@ -4,8 +4,6 @@ Package.describe({
 
 Package.on_use(function (api) {
     api.use(['jquery'], 'client');
-
-	api.add_files('lib/wysihtml5.js', 'client', {bare:true});  
 	api.add_files('lib/bootstrap-wysihtml5.css', 'client'); 
 	api.add_files('lib/wysihtml5-0.3.0.js', 'client', {bare:true});
 	api.add_files('lib/bootstrap3-wysiwyg5-color.css', 'client');

f082faee2cbde91959b15f2676ea12df1c27f3f4
Austin Rivas <austinrivas@gmail.com>
2014-03-03 20:28:49 -0800

    Update package.js

--- package.js
+++ package.js
@@ -8,5 +8,5 @@ Package.on_use(function (api) {
     api.add_files('lib/wysihtml5-0.3.0.js', 'client', {bare:true});
     api.add_files('lib/bootstrap3-wysiwyg5-color.css', 'client');
     api.add_files('lib/bootstrap3-wysihtml5.js', 'client', {bare:true});
-    api.export(['wysihtml'],['client']);
+    api.export(['wysihtml5'],['client']);
 });
